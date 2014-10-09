49d4e7791e0daa9de597e228cbc82c70e8e7c027
Matt Brown <matt.brown@mailtrust.com>
2014-10-09 15:57:19 -0400

    Add more browser prefixes for rs-popover-message

--- src/javascripts/module.js
+++ src/javascripts/module.js
@@ -6,7 +6,7 @@ angular.module('rs.popover', []).run(function () {
   styleContent = document.createTextNode('.rs-popover-overlay { position: fixed; top: 0; left: 0; right: 0; bottom: 0; opacity: 0 } \
     .rs-popover-loading, .rs-popover-error { width: 200px; height: 140px } \
     .rs-popover-error { color: #c40022 } \
-    .rs-popover-message { width: 100%; position: absolute; top: 50%; left: 50%; -webkit-transform: translate(-50%, -50%); transform: translate(-50%, -50%); text-align: center; } \
+    .rs-popover-message { width: 100%; position: absolute; top: 50%; left: 50%; -moz-transform: translate(-50%, -50%); -ms-transform: translate(-50%, -50%); -webkit-transform: translate(-50%, -50%); transform: translate(-50%, -50%); text-align: center; } \
     .rs-popover-body { margin: 0; padding: 20px }');
   styleTag = document.createElement('style');
   styleTag.type = 'text/css';
