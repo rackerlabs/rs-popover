95d6c5ab8a00f72a9a8b5e4259357f982299151c
Brad Gignac <bgignac@bradgignac.com>
2014-11-16 21:40:18 -0500

    fix(rsPopoverForm): add block styling for ng-form

--- src/javascripts/module.js
+++ src/javascripts/module.js
@@ -4,6 +4,7 @@ angular.module('rs.popover', []).run(function () {
   var styleContent, styleTag;
 
   styleContent = document.createTextNode('.rs-popover-overlay { position: fixed; top: 0; left: 0; right: 0; bottom: 0; opacity: 0 } \
+    .rs-popover-content { display: block; } \
     .rs-popover-loading, .rs-popover-error { width: 200px; height: 140px } \
     .rs-popover-error { color: #c40022 } \
     .rs-popover-message { width: 100%; position: absolute; top: 50%; left: 50%; -moz-transform: translate(-50%, -50%); -ms-transform: translate(-50%, -50%); -webkit-transform: translate(-50%, -50%); transform: translate(-50%, -50%); text-align: center; } \
